aefd9361231da86f217e9a0ffa121aeaca3d2ec3
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-11-07 22:39:25 +0700

    Java Spring Boot

---
+++ Springboot-tutorial/src/main/java/com/tutorial/apidemo/database/Database.java
@@ -0,0 +1,28 @@
+package com.tutorial.apidemo.database;
+
+import com.tutorial.apidemo.models.Product;
+import com.tutorial.apidemo.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Now connect with mysql using JPA
+@Configuration
+public class Database {
+    // logger
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+    @Bean
+     CommandLineRunner initDatabase(ProductRepository productRepository) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Product productA = new Product("MacBook Pro 15", 2020, 2200.0, "");
+                Product productB = new Product("IPad Pro 16", 2020, 2400.0, "");
+                logger.info("insert data: "+productRepository.save(productA));
+                logger.info("insert data: "+productRepository.save(productB));
+            }
+        };
+    }
+}
---
+++ Springboot-tutorial/src/main/java/com/tutorial/apidemo/models/Product.java
@@ -0,0 +1,81 @@
+package com.tutorial.apidemo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// POJO = Plan Object Java Object
+@Entity
+public class Product {
+    // this is "primary key"
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String productName;
+    private int productYear;
+    private Double price;
+    private String url;
+
+    // default constructor
+    public Product() {
+    }
+
+    public Product(String productName, int productYear, Double price, String url) {
+        this.productName = productName;
+        this.productYear = productYear;
+        this.price = price;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getYear() {
+        return productYear;
+    }
+
+    public void setYear(int productYear) {
+        this.productYear = productYear;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", productYear=" + productYear +
+                ", price=" + price +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
